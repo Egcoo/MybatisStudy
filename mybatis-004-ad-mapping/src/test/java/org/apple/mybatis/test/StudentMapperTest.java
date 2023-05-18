aaff4f1318c08a6ab551f06756e4c6e39de77423
Egcoo <2957448049@qq.com>
2023-05-18 13:09:36 +0800

    学生项目集成ehcache

--- mybatis-004-ad-mapping/src/test/java/org/apple/mybatis/test/StudentMapperTest.java
+++ mybatis-004-ad-mapping/src/test/java/org/apple/mybatis/test/StudentMapperTest.java
@@ -34,11 +34,22 @@ public class StudentMapperTest {
     //写一个名为testSelectByIdAssociation()的测试方法，并打印结果
     @Test
     public void testSelectByIdAssociation() throws IOException {
-        SqlSession sqlSession = SqlSessionUtil.openSqlSession();
-        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
-        Student student = mapper.selectByIdAssociation(1);
-        System.out.println(student);
-        sqlSession.close();
+        SqlSession sqlSession1 = SqlSessionUtil.openSqlSession();
+        SqlSession sqlSession2 = SqlSessionUtil.openSqlSession();
+        StudentMapper mapper1 = sqlSession1.getMapper(StudentMapper.class);
+        StudentMapper mapper2 = sqlSession2.getMapper(StudentMapper.class);
+
+        //这行代码执行结束之后,实际上数据是缓存到一级缓存当中了。(sqlSession1是一级缓存。)
+        Student student1 = mapper1.selectByIdAssociation(1);
+        System.out.println(student1);
+
+        //如果这里不关闭SqlSession1对象的话,二级缓存中还是没有数据的
+        //如果执行了这行代码,sqlSession1的一级缓存中的数据会放到二级缓存当中
+        sqlSession1.close();
+
+        //这行代码执行结束之后,实际上数据会缓存到一级缓存当中。(sqlSession2是一级缓存。)
+        Student student2 = mapper2.selectByIdAssociation(1);
+        System.out.println(student2);
     }
 
 
