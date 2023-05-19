a56bb0a815e8dfc54e688c52715cd2c835708778
Egcoo <2957448049@qq.com>
2023-05-19 11:15:25 +0800

    新增一个逆向工程模块

---
+++ mybatis-005-generate/src/main/java/com/apple/mybatis/pojo/Car.java
@@ -0,0 +1,63 @@
+package com.apple.mybatis.pojo;
+
+public class Car {
+    private Integer id;
+
+    private String carNum;
+
+    private String brand;
+
+    private Long guidePrice;
+
+    private String produceTime;
+
+    private String carType;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCarNum() {
+        return carNum;
+    }
+
+    public void setCarNum(String carNum) {
+        this.carNum = carNum == null ? null : carNum.trim();
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand == null ? null : brand.trim();
+    }
+
+    public Long getGuidePrice() {
+        return guidePrice;
+    }
+
+    public void setGuidePrice(Long guidePrice) {
+        this.guidePrice = guidePrice;
+    }
+
+    public String getProduceTime() {
+        return produceTime;
+    }
+
+    public void setProduceTime(String produceTime) {
+        this.produceTime = produceTime == null ? null : produceTime.trim();
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType == null ? null : carType.trim();
+    }
+}
---
+++ mybatis-005-generate/src/test/java/CarMapperTest.java
@@ -0,0 +1,8 @@
+/**
+ * @ author Egcoo
+ * @ date 2023/5/19 - 11:14
+ */
+public class CarMapperTest {
+
+
+}
